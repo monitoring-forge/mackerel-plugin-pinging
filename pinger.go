7e9d4e8ef4ee81e3b0c2a4ce4e7486825fdf40e8
Masahiro Nagano <kazeburo@gmail.com>
2026-08-18 10:23:01 +0900

    Update pinger.go
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pinger.go
+++ pinger.go
@@ -10,28 +10,20 @@ import (
 func resolveIPAddrAndPinger(addr string) (*net.IPAddr, *ping.Pinger, error) {
 	var ra *net.IPAddr
 	var pinger *ping.Pinger
-	if strings.Contains(addr, ":") {
-		r, err := net.ResolveIPAddr("ip6", addr)
-		if err != nil {
-			return ra, pinger, err
-		}
-		ra = r
-		p, err := ping.New("", "::")
-		if err != nil {
-			return ra, pinger, err
-		}
-		pinger = p
-	} else {
-		r, err := net.ResolveIPAddr("ip4", addr)
-		if err != nil {
-			return ra, pinger, err
-		}
-		ra = r
-		p, err := ping.New("0.0.0.0", "")
-		if err != nil {
-			return ra, pinger, err
-		}
-		pinger = p
+	r, err := net.ResolveIPAddr("ip", addr)
+	if err != nil {
+		return nil, nil, err
 	}
+
+	var pinger *ping.Pinger
+	if r.IP.To4() != nil {
+		pinger, err = ping.New("0.0.0.0", "")
+	} else {
+		pinger, err = ping.New("", "::")
+	}
+	if err != nil {
+		return r, nil, err
+	}
+	return r, pinger, nil
 	return ra, pinger, nil
 }
